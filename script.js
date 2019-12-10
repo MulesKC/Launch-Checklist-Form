8f1bd92fcfe888c2905144367839a710a7863a26
MulesKC <rmmcp3@gmail.com>
2019-12-09 20:24:28 -0600

    added load and submit event listeners, started validation of fields

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
 // Write your JavaScript code here!
+window.addEventListener("load", function(){
+   let form = document.getElementById("launchForm")
+
+   form.addEventListener("submit", function(event) {
+      let pilotVal = document.querySelector("input[name=pilotName]")
+
+      let copilotVal = document.querySelector("input[name=copilotName]")
+      
+      let fuelLevelVal = document.querySelector("input[name=fuelLevel]")
+
+      let cargoMassVal = document.querySelector("input[name=cargoMass]")
+
+      if (pilotVal.value.length == 0 || 
+         copilotVal.value.length == 0 || 
+         fuelLevelVal.value.length == 0 || 
+         cargoMassVal.value.length == 0) {
+            alert("forgot something!")
+            event.preventDefault()
+         }
+
+   })
+
+})
+
+
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
