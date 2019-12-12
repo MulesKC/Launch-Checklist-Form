224434713edb348e9fbbeb3ce5b20eb5fed6bae2
MulesKC <rmmcp3@gmail.com>
2019-12-11 20:44:24 -0600

    updated after Blake responses

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 // Write your JavaScript code here!
 window.addEventListener("load", function(){
+
+   let planetIndex = 4;
+
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
+      response.json().then( function(json) {
+
+         let missionTarget = document.getElementById("missionTarget")
+
+         missionTarget.innerHTML = `<h2>Mission Destination</h2>
+            <ol>
+               <li>Name: ${json[planetIndex]["name"]}</li>
+               <li>Diameter: ${json[planetIndex]["diameter"]}</li>
+               <li>Star: ${json[planetIndex]["star"]}</li>
+               <li>Distance from Earth: ${json[planetIndex]["distance"]}</li>
+               <li>Number of Moons: ${json[planetIndex]["moons"]}</li>
+            </ol>
+         <img src="${json[planetIndex]["image"]}"></img>`
+
+      })
+   })
+
    let form = document.getElementById("launchForm")
 
    form.addEventListener("submit", function(event) {
+
       let pilot = document.querySelector("input[name=pilotName]")
 
       let copilot = document.querySelector("input[name=copilotName]")
@@ -10,22 +32,27 @@ window.addEventListener("load", function(){
       let fuelLevel = document.querySelector("input[name=fuelLevel]")
 
       let cargoMass = document.querySelector("input[name=cargoMass]")
+      
+      
 
-      if (pilot.value.length == 0 || 
-         copilot.value.length == 0 || 
-         fuelLevel.value.length == 0 || 
-         cargoMass.value.length == 0) {
-            alert("forgot something!");
-            event.preventDefault();
-         }
+      // if (pilot.value.length == 0 || 
+      //    copilot.value.length == 0 || 
+      //    fuelLevel.value.length == 0 || 
+      //    cargoMass.value.length == 0) {
+      //       alert("forgot something!");
+      //       event.preventDefault();
+      //    }
 
-      if (isNaN(fuelLevel.value) ||
-         isNaN(cargoMass.value) ||
-         !isNaN(pilot.value) ||
-         !isNaN(copilot.value)) {
-         alert("invalid entry!");
-         event.preventDefault();
-      }
+      // let letterCheck = /^[a-zA-Z]+$/;
+      
+
+      // if (isNaN(fuelLevel.value) ||
+      //    isNaN(cargoMass.value) ||
+      //    !pilot.value.match(letterCheck) ||
+      //    !copilot.value.match(letterCheck)) {
+      //    alert("invalid entry!");
+      //    event.preventDefault();
+      // }
 
 
       //remove later
@@ -38,15 +65,16 @@ window.addEventListener("load", function(){
       document.querySelector("#copilotStatus").innerHTML = `Copilot ${copilot.value} Ready`
 
       let launchStatus = document.getElementById("launchStatus")
+
+      let ready = true
       
       if (Number(fuelLevel.value) < 10000) {
          document.getElementById("faultyItems").style.visibility = "visible"
          document.getElementById("fuelStatus").innerHTML = `Fuel: ${fuelLevel.value}. Not enough fuel!`
          launchStatus.innerHTML = "Shuttle not ready for launch"
          launchStatus.style.color = "red"
-      } else {
-         launchStatus.innerHTML = "Shuttle is ready for launch"
-         launchStatus.style.color = "green"
+         ready = false
+         event.preventDefault();
       }
 
       if (cargoMass.value > 10000) {
@@ -54,31 +82,15 @@ window.addEventListener("load", function(){
          document.getElementById("cargoStatus").innerHTML = `Shuttle is too heavy!`
          launchStatus.innerHTML = "Shuttle not ready for launch"
          launchStatus.style.color = "red"
-      } else {
-         launchStatus.innerHTML = "Shuttle is ready for launch"
-         launchStatus.style.color = "green"
+         ready = false
+         event.preventDefault();
       }
 
-      let planetIndex = randomNum()
-
-      fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
-         response.json().then( function(json) {
-
-            let missionTarget = document.getElementById("missionTarget")
-
-            missionTarget.innerHTML = `<h2>Mission Destination</h2>
-               <ol>
-                  <li>Name: ${json[planetIndex]["name"]}</li>
-                  <li>Diameter: ${json[planetIndex]["diameter"]}</li>
-                  <li>Star: ${json[planetIndex]["star"]}</li>
-                  <li>Distance from Earth: ${json[planetIndex]["distance"]}</li>
-                  <li>Number of Moons: ${json[planetIndex]["moons"]}</li>
-               </ol>
-            <img src="${json[planetIndex]["image"]}"></img>`
-
-         })
-      })
-
+      if (ready) {
+         launchStatus.innerHTML = "Shuttle is ready for launch"
+         launchStatus.style.color = "green"
+         event.preventDefault();
+      }
    })
 
 })
